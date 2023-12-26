364424b77469142c5efbcd49a4e80a0cbae5e95c
divyakrishna <kattadivyakrishna@gmail.com>
2023-12-26 22:30:53 +0530

    321

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -18,3 +18,12 @@ exit 1
 else
 echo "installing MYSQL is success"
 fi
+
+yum install git -y
+if [$? -ne 0]
+then
+echo "ERROR:: installing GIT is failed"
+exit 1
+else
+echo "installing GIT is success"
+fi
