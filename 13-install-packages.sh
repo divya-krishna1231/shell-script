926782ad60612025ae6562985ee9c87ed2aa6d05
divyakrishna <kattadivyakrishna@gmail.com>
2023-12-30 15:37:08 +0530

    321

--- 13-install-packages.sh
+++ 13-install-packages.sh
@@ -2,7 +2,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33mm"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+      echo -e "$2 ... $R FAILED $N"
+    else
+      echo -e "$2 ... $G success $N" 
+    fi   
+}
 
 if [ $ID -ne 0 ]
 then
@@ -12,4 +27,21 @@ else
      echo "yoy are root user"
 fi
 
-echo "All arguments passed : $@ "
+#echo "All arguments passed : $@ "
+
+for package in $@
+
+do
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ] 
+    then 
+       yum install $package -y
+       VALIDATE $? "Installation of $package"
+    else
+       echo -e " $Package is already installed.. $Y SKIPPING $N"
+    fi
+
+done        
+
+
+
