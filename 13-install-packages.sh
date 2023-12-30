5a3579a7faabdc1c5fcf95dd3ea28722d2010ee2
divyakrishna <kattadivyakrishna@gmail.com>
2023-12-30 15:06:51 +0530

    321

---
+++ 13-install-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+   echo  -e "$R ERROR:: please run this script with root access $N"
+     exit 1
+else
+     echo "yoy are root user"
+fi
+
+echo "All arguments passed : $@"
