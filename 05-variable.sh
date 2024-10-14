689ac8a0bbc6ef790ecfba43cc9361c053952e1c
Divya Krishna <kattadivyakrishna@gmail.com>
2024-10-14 18:17:14 +0530

    2nd script

---
+++ 05-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your username:: "
+
+read -s USERNAME 
+
+echo "Please enter your Password:: "
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
